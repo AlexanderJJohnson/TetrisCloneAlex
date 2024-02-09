16ea92af1cf6be28aecb5af04be94b5b0d4f134f
DrumlineBaller <aj34johnson2021@gmail.com>
2024-02-08 23:51:50 -0600

    Test Commit 2 for adding the purpose of this project

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+// I am currently attempting to build a basic tetris clone using my current knowledge of the C programming language.
+// My motivation for doing this project is to learn and push myself to get a functioning personal project done. 
+// The main problem that this project will tackle is making me a stronger and more proficient thinker.
+// I have had a lot of fun and some difficulties throughout building this project and hope to get it done soon. 
+// A further goal would be to optimize and implement this project on hardware.
+
+
 #include <stdio.h>
 #include <ncurses.h>
 #include <time.h>
 #include <stdlib.h>
+
 
 
 //globals
